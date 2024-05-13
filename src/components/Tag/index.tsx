48d4572659ab65703f703af8c80a5d138f660cf8
arildo <arildohp@gmail.com>
2024-05-13 10:11:53 -0300

    componentizando o botão aproveitar

---
+++ src/components/Tag/index.tsx
@@ -0,0 +1,10 @@
+import { TagContainer } from './styled'
+export type Props = {
+  size?: 'small' | 'big'
+  children: string
+}
+
+const Tag = ({ children, size = 'small' }: Props) => (
+  <TagContainer size={size}>{children}</TagContainer>
+)
+export default Tag
