9441e85770a9b56c0a7aa8a27c0bb05727ddfe67
arildo <arildohp@gmail.com>
2024-06-20 07:47:20 -0300

    melhorando a responsividade

--- src/components/ProductsList/styles.ts
+++ src/components/ProductsList/styles.ts
@@ -1,7 +1,7 @@
 import styled from 'styled-components'
 
 import { Props } from '.'
-import { cores } from '../../styles'
+import { breakpoints, cores } from '../../styles'
 import { Card } from '../Product/styles'
 
 export const Container = styled.section<Omit<Props, 'title' | 'games'>>`
@@ -21,6 +21,11 @@ export const List = styled.ul`
   grid-template-columns: 1fr 1fr 1fr 1fr;
   gap: 24px;
   margin-top: 40px;
+  padding-left: 0;
+
+  @media (max-width: ${breakpoints.desktop}) {
+    grid-template-columns: 1fr 1fr;
+  }
 `
 
 export const Title = styled.h2`
