b44872f3bcf84c6fb07eb03ae6fc1c1380662ccd
arildo <arildohp@gmail.com>
2024-06-22 07:43:50 -0300

    melhorando a responsividade

--- src/store/reducers/cart.ts
+++ src/store/reducers/cart.ts
@@ -1,5 +1,5 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit'
-import { Game } from '../../pages/home'
+import { Game } from '../../pages/Home'
 
 type CartState = {
   items: Game[]
