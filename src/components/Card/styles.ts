42c4a45b9334d3d21c7474f77569d9ec3537d84a
arildo <arildohp@gmail.com>
2024-06-23 09:17:14 -0300

    construindo a pagina de checkout

--- src/components/Card/styles.ts
+++ src/components/Card/styles.ts
@@ -6,4 +6,21 @@ export const Container = styled.div`
   background-color: ${cores.cinza};
   padding: 24px;
   margin-bottom: 40px;
+
+  h2,
+  h3 {
+    font-size: 18px;
+    font-weight: bold;
+    color: ${cores.branca};
+    margin-bottom: 24px;
+  }
+
+  .margin-top {
+    margin-top: 24px;
+  }
+
+  p {
+    font-size: 14px;
+    line-height: 22px;
+  }
 `
--- src/pages/Checkout/index.tsx
+++ src/pages/Checkout/index.tsx
@@ -1,23 +1,49 @@
+import { Button } from '../../components/Button'
 import Card from '../../components/Card'
+import { InputGroup, Row } from './styles'
 
 const Checkout = () => (
   <div className="container">
     <Card title="Dados de cobrança">
-      <div>
-        <div>
-          <label htmlFor="fullname">Nome container</label>
-          <input id="fullname" type="text" />
-        </div>
-        <div>
-          <label htmlFor="email">E-mail</label>
-          <input id="email" type="email" />
-        </div>
-        <div>
-          <label htmlFor="cpf">CPF</label>
-          <input id="cpf" type="text" />
-        </div>
-      </div>
+      <>
+        <Row>
+          <InputGroup>
+            <label htmlFor="fullname">Nome container</label>
+            <input id="fullname" type="text" />
+          </InputGroup>
+          <InputGroup>
+            <label htmlFor="email">E-mail</label>
+            <input id="email" type="email" />
+          </InputGroup>
+          <InputGroup>
+            <label htmlFor="cpf">CPF</label>
+            <input id="cpf" type="text" />
+          </InputGroup>
+        </Row>
+        <h3 className="margin-top">Dados da entrega - conteudo digital</h3>
+        <Row>
+          <InputGroup>
+            <label htmlFor="deliveryEmail">E-mail</label>
+            <input id="deliveryEmail" type="email" />
+          </InputGroup>
+          <InputGroup>
+            <label htmlFor="confirmDeliveryEmail">Confirme o e-mail</label>
+            <input id="confirmeDeliveryEmail" type="email" />
+          </InputGroup>
+        </Row>
+      </>
     </Card>
+    <Card title="pagamento">
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque
+        excepturi animi similique consequatur, debitis molestias quasi doloribus
+        laudantium nulla distinctio nostrum libero ipsum magni amet accusantium
+        totam iure consequuntur quidem.
+      </p>
+    </Card>
+    <Button type="button" title=" Cliqyue aqui para finalizar a compra">
+      Finalizar compra
+    </Button>
   </div>
 )
 export default Checkout
